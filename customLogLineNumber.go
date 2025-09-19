adab6b1cb340ac7ce2e4500f1ff03f15cecc4262
NeoVai <mirror.unix@proton.me>
2025-09-19 11:58:41 +0000

    customLogLineNumber

---
+++ customLogLineNumber.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+)
+
+func main() {
+	LOGFILE := path.Join(os.TempDir(), "mGo.log")
+	f, err := os.OpenFile(LOGFILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	iLog := log.New(f, "iLog ", log.LstdFlags&^log.Ldate|log.Lshortfile)
+
+	iLog.Println("Hello world")
+	iLog.Println("Mastering Golang")
+
+	iLog.SetFlags(log.LstdFlags | log.Llongfile)
+
+}
