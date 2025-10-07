9008cd51ab459b34aa78e634dd7b74b359b85316
NeoVai <mirror.unix@proton.me>
2025-10-07 20:50:28 +0000

    nilMap

--- ch03/nilMap.go
+++ ch03/nilMap.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Panic: ", r)
+		}
+	}()
+
+	aMap := map[string]int{}
+	aMap["test"] = 1
+	fmt.Println("aMap:", aMap)
+	aMap = nil
+
+	fmt.Println("aMap:", aMap)
+	if aMap == nil {
+		fmt.Println("nil map!")
+		aMap = map[string]int{}
+	}
+
+	aMap["test"] = 1
+
+	aMap = nil
+	aMap["test"] = 1
+	fmt.Println("aMap", aMap)
 }
